795fb80b29ce1879b6748740310aa28058780a6d
prabhat <sse.prabhatchy@gmail.com>
2022-11-11 11:30:14 +0530

    warehouse model added

---
+++ database/migrations/2022_11_10_111327_create_customer_warehouse_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerWarehouseTable extends Migration
+{
+    /**
+     * Run the migrations.  
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_warehouse', function (Blueprint $table) {
+            $table->id();
+            $table->string('wh_name');
+            $table->tinyInteger('wh_type')->comment("1=Own Warehouse 2=Own Showroom 3=Wholeseller Store/ Warehouse 4=Retailer Residen");
+            $table->string('customer_id')->nullable();
+            $table->string('customer_store_id')->nullable();
+            $table->string('customer_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('building')->nullable();
+            $table->string('sub_district')->nullable();
+            $table->string('district')->nullable();
+            $table->string('road')->nullable();            
+            $table->string('country_id')->nullable();
+            $table->string('state_id')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zipcode')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->string('tel_number')->nullable();
+            $table->tinyInteger('status')->default(1)->nullable();        
+            $table->timestamps();
+        });
+
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_warehouse');
+    }
+}
