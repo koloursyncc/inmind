94732a7cbeb207156d966fbd5045d673344b3473
Sonali <rautsonali03@gmail.com>
2022-12-22 18:25:46 +0530

    sonali changes

--- database/migrations/2022_12_22_125356_alter_po_invoice.php
+++ database/migrations/2022_12_22_125356_alter_po_invoice.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CustIdToPoInvoice extends Migration
+class AlterPoInvoice extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class CustIdToPoInvoice extends Migration
      */
     public function up()
     {
-        DB::statement("ALTER TABLE `po_invoice` ADD `cust_id` int(11) NULL DEFAULT NULL AFTER `po_id`;");
-         DB::statement("ALTER TABLE `po_invoice` ADD `status` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '1:Overdue, 0:Normal' AFTER `pay_this_time`");
+        Schema::table('po_invoice', function (Blueprint $table) {
+            $table->integer('cust_id');
+        });
+
+        Schema::table('po_invoice', function (Blueprint $table) {
+            $table->tinyInteger('status')->default(0)->comment('1:Overdue 0:Normal');
+        });
     }
 
     /**
@@ -24,6 +29,6 @@ class CustIdToPoInvoice extends Migration
      */
     public function down()
     {
-        
+        //
     }
 }
